15f5aac7445cbc2f2396dd88166e71f6aaaf3a73
puregyu <puregyu@naver.com>
2021-10-15 23:58:41 +0900

    expected final과 쉐도잉

--- src/main/java/onlyme/laboratory/java8/functionalInterface/SimpleMain.java
+++ src/main/java/onlyme/laboratory/java8/functionalInterface/SimpleMain.java
@@ -1,5 +1,7 @@
 package onlyme.laboratory.java8.functionalInterface;
 
+import java.util.function.Consumer;
+
 public class SimpleMain {
 
     public static void main(String[] args) {
@@ -13,6 +15,38 @@ public class SimpleMain {
 
         // 함수형 인터페이스를 구현할 때, 줄여서 사용
         SimpleInterface runSomethingByLambdaExpression = () -> System.out.println("hello");
+
+        run();
+    }
+
+    private static void run() {
+
+        // final 키워드가 없어도 로컬클래스, 익명클래스, 람다 모두 참조가 가능하다.
+        // 사실상 final 이여야 함.(expected final)
+        int baseNumber = 10;
+
+        // 로컬 클래스, 쉐도잉 가능
+        class LocalClass {
+            void printBaseNumber() {
+                int baseNumber = 11;
+                System.out.println(baseNumber); // 11
+            }
+        }
+
+        // 익명 클래스, 쉐도잉 가능
+        Consumer<Integer> consumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                int baseNumber = 11;
+                System.out.println(baseNumber); // 11
+            }
+        };
+
+        // 람다, 쉐도잉 불가능
+        Consumer<Integer> consumer2 = integer -> {
+//            int baseNumber = 11; // 람다는 run() 메소드와 같은 scope 라서 쉐도잉이 불가능함. 컴파일 에러 발생
+            System.out.println(baseNumber);
+        };
     }
 
 }
